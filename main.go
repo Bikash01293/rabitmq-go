bb883a10edd37d18aaf5ecf2214967574a0cd6cd
Bikash01293 <adhikaribikash01293@gmail.com>
2022-03-11 17:24:36 +0530

    new worker object has been created and returned, Start function starts to take the work request and an anonymous function stops the worker request after it has finished.

--- main.go
+++ main.go
@@ -51,6 +51,61 @@ func Collector(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+//Creating struct of Worker
+type Worker struct {
+	ID int
+	Work chan WorkRequest
+	WorkerQueue chan chan WorkerRequest
+	QuitChan chan bool
+
+}
+
+// NewWorker creates, and returns a new Worker object
+func NewWorker(id int, workerQueue chan chan WorkerRequest) Worker {
+	//Create and return the worker
+	worker := Worker {
+		ID: id,
+		Work: make(chan WorkRequest),
+		workerQueue: workerQueue,
+		QuitChan: make(chan bool)
+	}
+
+	return worker
+}
+
+// This function "starts" the worker by starting a goroutine, that is an infinite "for-select" loop.
+func (w *Worker) Start() {
+	go func() {
+		
+		for {
+			//Add ourselves into the worker queue.
+			w.WorkerQueue <- w.Work
+
+			select {
+			case work := <- w.Work:
+				//Recieve a work request.
+				fmt.Printf("worker%d: Recieved work request, delaying for %f seconds", w.Id, work.delay.Seconds())
+
+				time.Sleep(work.Delay)
+				fmt.Printf("worker%d: Hello, %s!\n", w.ID, work.Name)
+
+			case <-w.QuitChan:
+				//We have been asked to stop
+				fmt.Printf("worker%d stopping\n", w.ID)
+				return
+			}
+		}
+	}()
+}
+
+//Stop tells the worker to stop listening for work requests.
+//Note that the worker will only stop "after" it has finished its work.
+func (w *Worker) Stop() {
+	go func() {
+		w.QuitChan <- true
+	}
+}()
+
 var (
 	Nworkers = flag.Int("n", 4, "The number of workers to start")
 	HTTPAddr = flag.String("http", "127.0.0.1:8080", "Address to listen for HTTP requests on")
